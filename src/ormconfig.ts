44c8fce5f416defd947c50fd70abfe743fe513e6
Chu Van Gia <vangiachu@gmail.com>
2023-04-16 09:17:53 +0700

    Up project to Github to fix error.

---
+++ src/ormconfig.ts
@@ -0,0 +1,14 @@
+import { PostgresConnectionOptions } from "typeorm/driver/postgres/PostgresConnectionOptions";
+
+const config: PostgresConnectionOptions = {
+  type: 'postgres',
+  host: 'localhost',
+  port: 5432,
+  username: 'mediumclone',
+  password: '123456',
+  database: 'mediumclone',
+  entities: [__dirname + '/**/*.entity{.ts,.js}'],
+  synchronize: true,
+};
+
+export default config;
