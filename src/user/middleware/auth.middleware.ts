8477e9c1f8572acb8bcad11e53a6b845c14299da
Chu Van Gia <vangiachu@gmail.com>
2023-04-16 23:12:35 +0700

    Commit 16.4.2023

---
+++ src/user/middleware/auth.middleware.ts
@@ -0,0 +1,30 @@
+import { JWT_SECRET } from "../../config";
+import { ExpressRquest } from "@app/types/expressRequest.interface";
+import { Injectable, NestMiddleware } from "@nestjs/common";
+import { NextFunction, Response } from "express";
+import { verify } from "jsonwebtoken";
+import { UserService } from "../user.service";
+
+@Injectable()
+export class AuthMiddleware implements NestMiddleware {
+  constructor(private readonly userService: UserService) {}
+  async use(req: ExpressRquest, _: Response, next: NextFunction) {
+  if (!req.headers.authorization) {
+    req.user = null;
+    next();
+    return;
+  }
+
+  const token = req.headers.authorization.split(' ')[1];
+  
+  try {
+    const decode = verify(token, JWT_SECRET);
+    const user = await this.userService.findById(decode.id);
+    req.user = user;
+    next();
+  } catch (err) {
+    req.user = null;
+    next();
+  }
+ }
+}
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,8 +1,14 @@
-import {Injectable} from "@nestjs/common";
+import {HttpException, HttpStatus, Injectable} from "@nestjs/common";
 import { CreateUserDto } from "./dto/createUser.dto";
 import { UserEntity } from "./user.entity";
 import { InjectRepository } from "@nestjs/typeorm";
 import { Repository } from "typeorm";
+import {sign} from 'jsonwebtoken';
+import { JWT_SECRET } from "./../config";
+import { UserResponseInterface } from "./types/userResponse.interface";
+import { LoginUserDto } from "./dto/loginUser.dto";
+import { compare } from "bcrypt";
+import { UpdateUserDto } from "./dto/updateUser.dto";
 
 @Injectable()
 export class UserService {
@@ -10,10 +16,87 @@ export class UserService {
     @InjectRepository(UserEntity)
     private readonly userRepository: Repository<UserEntity>,
   ) {}
-  async createUser(createUserDto: CreateUserDto) {
+  async createUser(createUserDto: CreateUserDto): Promise<UserEntity> {
+    const userByEmail = await this.userRepository.findOne({
+      where: { username: createUserDto.username },
+    });
+    const userByUsername = await this.userRepository.findOne({
+      where: { email: createUserDto.email },
+    });
+    if (userByEmail || userByUsername) {
+      throw new HttpException(
+        'Email or username are taken', 
+        HttpStatus.UNPROCESSABLE_ENTITY,
+      );
+    }
     const newUser = new UserEntity();
     Object.assign(newUser, createUserDto);
     console.log('newUser', newUser);
     return await this.userRepository.save(newUser);
+  }
+
+  async findById(id: number): Promise<UserEntity> {
+    return this.userRepository.findOne({ where: {id} });
+  }
+
+  async login(loginUserDto: LoginUserDto): Promise<UserEntity> {
+    const user = await this.userRepository.findOne({
+      where: {
+        email: loginUserDto.email,
+      },
+      select: ['id', 'username', 'email', 'bio', 'image', 'password'],
+    });
+    console.log('user', user);
+
+    if (!user) {
+      throw new HttpException(
+        'Credentials are not valid', 
+        HttpStatus.UNPROCESSABLE_ENTITY
+      );
+    }
+
+    const isPasswordCorrect = await compare(
+      loginUserDto.password, 
+      user.password
+      );
+
+      if (!isPasswordCorrect) {
+      throw new HttpException(
+        'Credentials are not valid', 
+        HttpStatus.UNPROCESSABLE_ENTITY,
+      );
+    }
+
+    delete user.password;
+    return user;
+  }
+
+  async updateUser(
+    userId: number, 
+    updateUserDto: UpdateUserDto,
+    ): Promise<UserEntity> {
+    const user = await this.findById(userId);
+    Object.assign(user, updateUserDto);
+    return await this.userRepository.save(user);
+  }
+
+  generateJwt(user: UserEntity): string {
+   return sign(
+    {
+    id: user.id,
+    username: user.username,
+    email: user.email,
+   }, 
+   JWT_SECRET,
+   ); 
+  }
+
+  buildUserResponse(user: UserEntity): UserResponseInterface {
+    return {
+      user: {
+        ...user,
+        token: this.generateJwt(user)
+      }
+    }
   }
 }
