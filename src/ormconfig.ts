c6ac92ec5e30c17e9f3cdbad803e46ec85a9dc72
Chu Van Gia <vangiachu@gmail.com>
2023-04-16 15:57:33 +0700

    Fix typo bug

--- src/ormconfig.ts
+++ src/ormconfig.ts
@@ -8,7 +8,8 @@ const config: PostgresConnectionOptions = {
   password: '123456',
   database: 'mediumclone',
   entities: [__dirname + '/**/*.entity{.ts,.js}'],
-  synchronize: true,
+  synchronize: false,
+  migrations: [__dirname + '/migrations/**/*{.ts, .js}'],
 };
 
 export default config;
